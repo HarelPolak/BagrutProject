3ed46ba9bcb060f486c3a38bac1786d590d07024
harelp <harel.polak@gmail.com>
2024-03-31 11:18:27 +0300

    timer touch listener

--- app/src/main/java/com/example/bagrutproject/TimerFragment.java
+++ app/src/main/java/com/example/bagrutproject/TimerFragment.java
@@ -1,14 +1,20 @@
 package com.example.bagrutproject;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class TimerFragment extends Fragment {
+public class TimerFragment extends Fragment implements View.OnTouchListener {
+
+    TextView tvTimer;
 
     public TimerFragment(){
         // require a empty public constructor
@@ -18,5 +24,25 @@ public class TimerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_timer, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        tvTimer = getView().findViewById(R.id.tvTimer);
+        tvTimer.setOnTouchListener(this);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if(view == tvTimer){
+            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                tvTimer.setTextColor(Color.GREEN);
+            }
+            else if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                tvTimer.setTextColor(Color.WHITE);
+            }
+        }
+        return false;
     }
 }
